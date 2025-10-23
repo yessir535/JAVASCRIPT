7b1c914d90d4bf28d8c9ea81cad3d332e96c8148
yessir535 <alekswiszcz@gmail.com>
2025-10-23 08:44:12 +0200

    1 lekcja

---
+++ script.js
@@ -0,0 +1,15 @@
+function Zmien_kolor(){
+document.getElementById("Imie").style.color = "red"
+}
+
+function Zmien_kolor_zielony(){
+    document.getElementById("imie").style.color = "green"
+}
+
+function kolor_szkola(){
+    document.getElementById("szkola").style.color = "blue"
+}
+
+function kolor_pogoda(){
+    document.getElementById("pogoda").style.color = "pink"
+}
