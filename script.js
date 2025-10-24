7ec5668d4568f76d5a9a6715d890e5b64b8f32f4
yessir535 <alekswiszcz@gmail.com>
2025-10-24 14:50:53 +0200

    Te najlepsze

---
+++ script.js
@@ -0,0 +1,31 @@
+function Zmien_kolor() {
+  document.getElementById("czerwony").style.color = "red";
+  document.getElementById("imie").style.color = "red";
+}
+ 
+function Zmien_kolor_zielony() {
+  document.getElementById("imie").style.color = "green";
+  document.getElementById("zielony").style.color = "green";
+}
+ 
+function Zmien_tekst_zielony() {
+  document.getElementById("zielony").innerHTML = "Zielony";
+}
+ 
+function Wroc_tekst_czerwony() {
+  document.getElementById("czerwony").style.color = "black";
+}
+ 
+function Wroc_tekst_zielony() {
+  document.getElementById("zielony").innerHTML = "Zmień kolor";
+  document.getElementById("zielony").style.color = "black";
+}
+function Zmien_tekst_czerwony() {
+  document.getElementById("czerwony").innerHTML = "Czerwony";
+}
+function Wroc_tekst_czerwony() {
+  document.getElementById("czerwony").innerHTML = "Zmień kolor";
+  document.getElementById("czerwony").style.color = "black";
+}
+
+ 
