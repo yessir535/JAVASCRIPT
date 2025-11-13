9708841a7cf96675b9a7a0ab3c583463db59fd85
yessir535 <alekswiszcz@gmail.com>
2025-11-13 08:45:05 +0100

    Add files via upload

---
+++ app13.10.js
@@ -0,0 +1,7 @@
+function wyswietl(){
+    var imie = document.querySelector("#imie").value
+    var nazwisko = document.getElementById("nazwisko").value
+
+    document.querySelector("h1").innerHTML = "Witaj     " + imie + "  " + nazwisko
+
+}
